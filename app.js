8423e4eba05ed6c12ecc937a476c558829723559
Zubair Naseer <zubairnaseer88@gmail.com>
2020-01-04 15:55:29 +0500

    sign up but not connect to BD

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser  = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+const signupRoute = require('./routes/signup');
+
+app.use('/signup', signupRoute);
+
+mongoose.connect(process.env.DB_Connection,{useUnifiedTopology: true,useNewUrlParser: true },() => 
+    console.log('connected to DB!')
+);
+
+app.listen(3000);
+ 
---
+++ routes/signup.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const Sign = require('../models/sign');
+
+router.post('/', (req,res)=>{
+    
+})
+
+module.exports = router;
